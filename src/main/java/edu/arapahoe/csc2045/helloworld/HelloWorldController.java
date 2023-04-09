45dba89fcb4b25fbcd2dd2d51cbb2abc71cac038
redwa <redwarr65@gmail.com>
2023-04-08 22:00:26 -0600

    Hello

---
+++ src/main/java/edu/arapahoe/csc2045/helloworld/HelloWorldController.java
@@ -0,0 +1,16 @@
+package edu.arapahoe.csc2045.helloworld;
+
+//public class HelloWorldController {
+//}
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/test")
+    public String index() {
+        return "Hello World from ACC!";
+    }
+
+}
